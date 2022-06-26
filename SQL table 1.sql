ab4d85fac36df68fa80b8d1b9042d7583e0bd489
Nekpak134 <105513365+Nekpak134@users.noreply.github.com>
2022-06-26 02:59:08 -0700

    Add files via upload

---
+++ SQL table 1.sql
@@ -0,0 +1,7 @@
+use nekpal
+select*from stmarks
+insert stmarks values (7,108,'sohan','hindi',80,'')
+insert stmarks values (8,110,'sachin','math',50,'10')
+insert stmarks values (3,112,'nekpal','histry',88,'12')
+insert stmarks values (4,116,'rihan','dml',77,'bca')
+delete from stmarks where id=4
